feff09522dacfde034ce1a138be63132329bbacb
unknown <felipe.soares@PC040.opencorporate.com.br>
2016-10-31 18:52:12 -0200

    ionic2

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    if (typeof($.fn.mask) == "function") {
-        $("input[data-mask='cpf']").mask("999.999.999-99");
-    }
-    $('.selectpicker').selectpicker({
-      dropupAuto: false
-    });
-});
